45f82c247ad176bd3dc57d266cc3d7f966fd6e39
silliw-nhoj <silliw.nhoj@gmail.com>
2020-10-09 10:29:17 -0700

    appvs41 irule

--- appvs41_irule.tcl
+++ appvs41_irule.tcl
@@ -1,5 +1,6 @@
 when HTTP_REQUEST {
   HTTP::header insert X-user-full-name "[ ACCESS::session data get session.oauth.client.last.id_token.name ]"
   HTTP::header insert X-user-email "[ ACCESS::session data get session.oauth.client.last.id_token.upn ]"
-  HTTP::header insert X-token-type "[ ACCESS::session data get session.oauth.client.last.id_token.typ ]"
+  HTTP::header insert X-token-type "[ ACCESS::session data get session.oauth.client.last.token_type ]"
+  HTTP::header insert X-token-id-type "[ ACCESS::session data get session.oauth.client.last.id_token.typ ]"
 }
