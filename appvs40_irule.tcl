20065fc509757d25708f3a5800f29631e5e78326
silliw-nhoj <silliw.nhoj@gmail.com>
2020-10-27 08:42:37 -0700

    add comment

--- appvs40_irule.tcl
+++ appvs40_irule.tcl
@@ -1,5 +1,6 @@
 when HTTP_REQUEST {
   if { [HTTP::header exists "Referer"] } {
+    # appvs40 referer replace
     HTTP::header replace Referer "https://[ ACCESS::session data get session.server.network.name ]"
   }
 }
